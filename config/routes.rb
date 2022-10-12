baecced008f201fdb010958f7066e722a3c91214
zeeshanale <zeeshan.alec1@gmail.com>
2022-10-12 13:56:20 +0500

    Added all articles listing feature (index page)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'pages#homepage'
   get '/about', to: 'pages#about'
 
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 
 end
