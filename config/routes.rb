dfac12ef2a1f9fd4fe4dd79af4ed5dc2bedf9b3b
zeeshanale <zeeshan.alec1@gmail.com>
2022-10-16 10:51:20 +0500

    Added users create action

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   resources :articles
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+
+  resources :users, except: [:new]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_15_081012) do
+ActiveRecord::Schema.define(version: 2022_10_16_031515) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_10_15_081012) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
