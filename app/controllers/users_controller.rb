7c15807e6280c179865314030c33c8228a1964de
zeeshanale <zeeshan.alec1@gmail.com>
2022-10-17 07:39:57 +0500

    Added login functionality and modified some links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+
     def new
         @user = User.new
     end
@@ -8,16 +10,16 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
+        
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def edit
-        @user = User.find(params[:id])
+        
     end
 
     def update
-        @user = User.find(params[:id])
+        
         if @user.update(user_params)
             flash[:notice] = "Profile updated"
             redirect_to user_path
@@ -29,6 +31,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed in"
             redirect_to articles_path
         else
@@ -39,6 +42,10 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
 end
